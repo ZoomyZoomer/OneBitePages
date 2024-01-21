0b0c302a265e217ffc39911dd97adb90b8a43b97
Kamil Wisniewski <realisticallyspeaking101@gmail.com>
2024-01-21 17:38:40 -0500

    based

--- blogsite/vite.config.js
+++ blogsite/vite.config.js
@@ -4,13 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        '@fortawesome/free-solid-svg-icons',
-        '@fortawesome/react-fontawesome',
-        '@fortawesome/fontawesome-svg-core',
-      ],
-    },
+  optimizeDeps: {
+    include: ['@fortawesome/react-fontawesome'],
   },
 });
