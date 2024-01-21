de2714bf17637e6cfcbffa48fa6ea72d0afac8a7
Kamil Wisniewski <realisticallyspeaking101@gmail.com>
2024-01-21 18:52:22 -0500

    3

--- blogsite/src/pages/EditPost.jsx
+++ blogsite/src/pages/EditPost.jsx
@@ -34,7 +34,7 @@ export default function EditPost() {
     if (files?.[0]) {
       data.set('file', files?.[0]);
     }
-    const response = await fetch('https://one-bite-pages.vercel.app/post', {
+    const response = await fetch('https://one-bite-pages.vercel.app/api/post', {
       method: 'PUT',
       body: data,
       credentials: 'include',
