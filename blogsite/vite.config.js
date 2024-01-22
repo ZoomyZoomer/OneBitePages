93d6583440c5ff66fb45a8346c471df8f4e0e205
Kamil Wisniewski <realisticallyspeaking101@gmail.com>
2024-01-21 19:51:06 -0500

    x'

--- blogsite/vite.config.js
+++ blogsite/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['@fortawesome/react-fontawesome'],
+    include: ['@fortawesome/react-fontawesome', 'axios'],
   },
 });
