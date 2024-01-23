29429d3cff8ea218f92b451a9af43f6e5a90c7b2
Kamil Wisniewski <realisticallyspeaking101@gmail.com>
2024-01-22 21:07:46 -0500

    x

--- blogsite/src/pages/CreatePost.jsx
+++ blogsite/src/pages/CreatePost.jsx
@@ -25,7 +25,7 @@ export default function CreatePost(){
         data.set('file', files[0]);
             
         e.preventDefault();
-        const response = await fetch("https://one-bite-pages.vercel.app/api/post", {
+        const response = await fetch("76.76.21.123:443/api/post", {
             method: 'POST',
             body: data,
             credentials: 'include',
